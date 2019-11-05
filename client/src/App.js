6aecde9ce9b0c1dcd05c8b0b8dee5fa8af0caa81
jon <jcrossguitar@gmail.com>
2019-11-04 21:00:53 -0800

    add alert

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Alert } from 'reactstrap';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Alert color="secondary">
+        This is a secondary alert — check it out!
+        </Alert>
         <a
           className="App-link"
           href="https://reactjs.org"
