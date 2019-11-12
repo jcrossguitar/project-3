54a3b7605d1cce5c50f321f6041743b6608d535f
jon <jcrossguitar@gmail.com>
2019-11-11 16:36:55 -0800

    matt and adam helped resolve the server issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-run'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home } from './components/Home/Home.component';
 
-import { About } from './components/About/About.component';
+import About  from './components/About/About.component.js';
 import { Alert } from 'reactstrap';
-import { NoMatch } from './components/NoMatch';
+// import { NoMatch } from './components/NoMatch/index.js';
 import { CheeseDetail } from './pages/CheeseDetail/CheeseDetail.component';
 
 function App() {
   return (
   <Router>
-    <switch>
-      <route exact path="/" component={Home} />
-      <route exact path="/about" component={About} />
-      <route exact path="/cheese:id" component={CheeseDetail} />
-      <route component={NoMatch} />
-    </switch>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/cheese:id" component={CheeseDetail} />
+      {/* <route component={NoMatch} /> */}
+    </Switch>
   </Router>
   );
 }
