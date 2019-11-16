6be015699b3b54d81a05bfd81731e4d2eef40780
Rob Adrain <radrainjr21712@Kristins-MacBook-Pro.local>
2019-11-16 14:33:44 -0800

    added Auth0 login button to home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useAuth0 } from "./react-auth0-spa";
+import NavBar from "./components/Nav/NavBar";
 import { Home } from './components/Home/Home.component';
 import { About } from './components/About/About.component';
 import { CheeseDetail } from './pages/CheeseDetail/CheeseDetail.component';
@@ -13,23 +15,39 @@ import { SalesPage } from './pages/SalesPage/SalesPage.component';
 import { ExamplePage } from './pages/ExamplePage/ExamplePage.component';
 import { NoMatch } from './components/NoMatch';
 
+
 function App() {
+
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/inventory" component={InventoryPage} />
-        <Route exact path="/assembly" component={AssemblyPage} />
-        <Route exact path="/sales" component={SalesPage} />
-        <Route exact path="/Qc" component={QcPage} />
-        <Route exact path="/shipping" component={ShippingPage} />
-        <Route exact path="/cheese/:id" component={CheeseDetail} />
-        <Route exact path="/example/:id" component={ExamplePage} />
-        <Route component={NoMatch} />
-      </Switch>
-    </Router>
-  );
+      <div className="App">
+        <header>
+          <NavBar />
+        </header>
+      </div>
+    );
+
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" component={Home} />
+    //     <Route exact path="/about" component={About} />
+    //     <Route exact path="/inventory" component={InventoryPage} />
+    //     <Route exact path="/assembly" component={AssemblyPage} />
+    //     <Route exact path="/sales" component={SalesPage} />
+    //     <Route exact path="/Qc" component={QcPage} />
+    //     <Route exact path="/shipping" component={ShippingPage} />
+    //     <Route exact path="/cheese/:id" component={CheeseDetail} />
+    //     <Route exact path="/example/:id" component={ExamplePage} />
+    //     <Route component={NoMatch} />
+    //   </Switch>
+    // </Router>
+
 }
 
 export default App;
