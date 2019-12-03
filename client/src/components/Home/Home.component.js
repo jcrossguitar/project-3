0fb5205de32dadadbbac78027f4d17ff37c3e68d
jon <jcrossguitar@gmail.com>
2019-12-02 18:30:47 -0800

    Updated package.json

--- client/src/components/Home/Home.component.js
+++ client/src/components/Home/Home.component.js
@@ -11,21 +11,6 @@ export const Home = props => {
 				<img src="/image/Eye.png" alt="all-seeing eye" width="400" ></img>
 				<h1 className="title">sauron</h1>
 			</a>
-			
 		</div>
-	);
-// 	const { loading } = useAuth0();
-
-//   if (loading) {
-//     return (
-//       <div>Loading...</div>
-//     );
-//   }
-//   return (
-//       <div className="App">
-//         <header>
-//           <NavBar />
-//         </header>
-//       </div>
-//     );
+			)
 };
